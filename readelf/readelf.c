a0b485be0faef77f9e53b7b97dc5d5e6f7576b6c
ZhaoYanguang <1009550274@qq.com>
2022-03-24 14:32:18 +0800

    lab1-1-exam-4

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -71,7 +71,7 @@ int readelf(u_char *binary, int size)
 	for (int i = 0;i < sh_entry_count;i++) {
 		shdr = (Elf32_Shdr*) ptr_sh_table;
 		//printf("Read:%d:0x%x,0x%x\n", i, shdr->sh_offset, shdr->sh_addr);
-		printf("Read:%d:0x%x,0x%x\n", i, shdr->sh_offset, shdr->sh_addr);
+		printf("Read:%d:0x%x,0x%x\n", i, shdr->sh_offset, shdr->sh_offset + shdr->sh_addr);
 		ptr_sh_table += sh_entry_size;
 	}
 /*wrong	ptr_sh_table += sh_entry_size*2;
