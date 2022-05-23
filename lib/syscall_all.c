d578cb0ad31510a302aa96f4a07852d5566c8e71
ZhaoYanguang <1009550274@qq.com>
2022-05-23 21:28:20 +0800

    1

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -457,6 +457,13 @@ int sys_ipc_can_send(int sysno, u_int envid, u_int value, u_int srcva,
 int sys_write_dev(int sysno, u_int va, u_int dev, u_int len)
 {
         // Your code here
+	if (!( dev >= 0x10000000 && dev <= 0x10000020 ||
+	       dev >= 0x13000000 && dev <= 0x13004200 ||
+	       dev >= 0x15000000 && dev <= 0x15000200)) {
+		return -E_INVAL;
+	}
+	bcopy((void *)va, (void *)(dev + 0xA0000000), len);
+	return 0;
 }
 
 /* Overview:
@@ -479,4 +486,11 @@ int sys_write_dev(int sysno, u_int va, u_int dev, u_int len)
 int sys_read_dev(int sysno, u_int va, u_int dev, u_int len)
 {
         // Your code here
+	if (!( dev >= 0x10000000 && dev <= 0x10000020 ||
+               dev >= 0x13000000 && dev <= 0x13004200 ||
+               dev >= 0x15000000 && dev <= 0x15000200)) {
+                return -E_INVAL;
+        }
+	bcopy((void *)(dev + 0xA0000000), (void *)va, len);
+	return 0;
 }
