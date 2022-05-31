673e48275d21c30b1a37438e7f0c3cdf62e71348
ZhaoYanguang <1009550274@qq.com>
2022-05-31 11:26:15 +0800

    1

--- fs/ide.c
+++ fs/ide.c
@@ -29,10 +29,32 @@ ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 	int offset_begin = secno * 0x200;
 	int offset_end = offset_begin + nsecs * 0x200;
 	int offset = 0;
-
+	int dev = 0x13000000;
+	int tmp;
 	while (offset_begin + offset < offset_end) {
 		// Your code here
 		// error occurred, then panic.
+		if (syscall_write_dev(&diskno, (dev + 0x10), 4) < 0) {
+         	       user_panic("ide_read wrong!!");
+        	}
+		tmp = offset_begin + offset;
+		if (syscall_write_dev(&tmp, dev, 4) < 0) {
+			user_panic("ide_read wrong!!");
+		}		
+		tmp = 0;
+		if (syscall_write_dev(&tmp, (dev + 0x20), 1) < 0) {
+			user_panic("ide_read wrong!!");
+		}
+		if (syscall_read_dev(&tmp, (dev + 0x30), 1) < 0) {
+			user_panic("ide_read wrong!!");
+		}
+		if (tmp == 0) [
+			user_panic("ide_read wrong at read!!");
+		}
+		if (syscall_read_dev((u_int)(dst + offset), (dev + 0x4000), 0x200) < 0) {
+			user_panic("ide_read wrong!!");
+		}
+		offset += 0x200;
 	}
 }
 
@@ -56,15 +78,40 @@ ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 {
 	// Your code here
 	// int offset_begin = ;
+	int offset_begin = secno * 0x200;
 	// int offset_end = ;
+	int offset_end = offset_begin + nsecs * 0x200;
 	// int offset = ;
-
+	int offset = 0;
+	int dev = 0x13000000;
+	int tmp;
 	// DO NOT DELETE WRITEF !!!
 	writef("diskno: %d\n", diskno);
 
-	// while ( < ) {
+	while (offset_begin + offset < offset_end) {
 		// copy data from source array to disk buffer.
-
+		if (syscall_write_dev((u_int)(src + offset), (dev + 0x4000), 0x200) < 0) {
+                        user_panic("ide_read wrong!!");
+                }
+		if (syscall_write_dev(&diskno, (dev + 0x10), 4) < 0) {
+                       user_panic("ide_read wrong!!");
+                }
+                tmp = offset_begin + offset;
+                if (syscall_write_dev(&tmp, dev, 4) < 0) {
+                        user_panic("ide_read wrong!!");
+                }
+                tmp = 1;
+                if (syscall_write_dev(&tmp, (dev + 0x20), 1) < 0) {
+                        user_panic("ide_read wrong!!");
+                }
+                if (syscall_read_dev(&tmp, (dev + 0x30), 1) < 0) {
+                        user_panic("ide_read wrong!!");
+                }
+                if (tmp == 0) [
+                        user_panic("ide_read wrong at read!!");
+                }		
+		offset += 0x200;
 		// if error occur, then panic.
-	// }
+	}
+
 }
